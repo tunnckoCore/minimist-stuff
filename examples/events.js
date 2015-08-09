317797cd1ce67190eccbbd1af0a04823aff6aa68
tunnckoCore <mameto_100@mail.bg>
2015-08-09 16:26:08 +0300

    add lib/events.js example

--- examples/events.js
+++ examples/events.js
@@ -1,3 +1,24 @@
 /* jshint asi:true */
 
 'use strict'
+
+var lib = require('../index')
+var assert = require('assert')
+var minimist = require('minimist')
+
+var cli = lib.events(minimist, {a: 'b'})
+
+cli
+  .on('help', function usage (self, opts) {
+    console.log(self, opts)
+    console.log('=========')
+    console.log('usage help')
+  })
+  .on('foo', function (val) {
+    console.log('foo:', val)
+  })
+  .on('h', function (val) {
+    this.emit('help', this, this.options)
+  })
+
+cli(process.argv.slice(2), {help: true})

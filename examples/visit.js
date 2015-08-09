3edaafe4be7b8f95d3f431674decd20886b9a319
tunnckoCore <mameto_100@mail.bg>
2015-08-09 20:03:09 +0300

    update `examples/visit.js`

--- examples/visit.js
+++ examples/visit.js
@@ -14,18 +14,19 @@ var cli = lib.plugins(events, {foo: 'bar', app: app})
 cli
   .on('set', console.log.bind(console, '[set]'))
   .on('get', console.log.bind(console, '[get]'))
-  // .on('*', function (key, val) {
-  //   console.log('*', key, val)
-  // })
   .on('help', function () {
     console.log('usage help')
   })
+
 cli
   .use(lib.visit)
 
 // node examples/visit.js
 // node examples/visit.js --set=a:123
 // node examples/visit.js --get=a
-cli(process.argv.slice(2), {help: true})
+// node examples/visit.js --set=foo:bar --set=a:123 --set=qux
+cli(process.argv.slice(2), {help: true, toBoolean: true})
 
+// it is able after `cli()`
 console.log(app.cache)
+//=> { foo: 'bar', a: 123, qux: true }

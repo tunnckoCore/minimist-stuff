48735c7a37003cd6fa113b9ebf19efe6e7f81d05
tunnckoCore <mameto_100@mail.bg>
2015-08-11 16:21:47 +0300

    lib/namespace.js and example

---
+++ examples/namespace.js
@@ -0,0 +1,35 @@
+/* jshint asi:true */
+
+'use strict'
+
+var App = require('./app').App
+var one = new App()
+var two = new App()
+
+var lib = require('../index')
+var minimist = require('minimist')
+var expand = lib.expand(minimist, {help: true})
+var events = lib.events(expand, {toBoolean: true, help: true})
+var cli = lib.namespace(events, {
+  namespace: {
+    one: one
+  }
+})
+
+cli
+  .on('help', function usage (self, opts) {
+    console.log('usage help')
+  })
+  .on('h', function (val) {
+    this.emit('help', this, this.options)
+  })
+  .on('one.set', console.log.bind(console, '[one.set]'))
+  .on('two.set', console.log.bind(console, '[two.set]'))
+
+// node examples/namespace.js --one.set=foo:bar --one.set=baz:qux
+// node examples/namespace.js --two.set=a:b --one.set=foo:bar
+cli(process.argv.slice(2), {
+  namespace: {
+    two: two
+  }
+})
